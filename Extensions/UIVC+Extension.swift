cf8133f0ba5342cdfa8aeb1550222a6834e1a6a2
Name <usmanovruslan2801@icloud.com>
2024-01-31 19:02:31 +0300

    iosdt HomerWork 3 FireBase Auth

---
+++ Extensions/UIVC+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  UIVC+Extension.swift
+//  Navigation
+//
+//  Created by Руслан Усманов on 31.01.2024.
+//
+
+import Foundation
---
+++ StorageService/Model.swift
@@ -0,0 +1,13 @@
+//
+//  model.swift
+//  Navigation
+//
+//  Created by Руслан Усманов on 24.01.2024.
+//
+
+import FirebaseAuth
+
+struct UserFirebase {
+    let user: User
+    
+}
